7dde1b0f47ca406179302330e007099a0fd2faf4
flauntycoot <70581513+flauntycoot@users.noreply.github.com>
2020-10-26 14:49:10 +0300

    Add files via upload

---
+++ src/com/company/TestDog.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class TestDog {
+    public static void main(String[] args) {
+        Dog d1 = new Dog("Mike", 3);
+        Dog d2 = new Dog("Helen", 8);
+        Dog d3 = new Dog("Bob");
+        d3.setAge(1);
+        System.out.println(d1);
+        d1.intoHumanAge();
+        d2.intoHumanAge();
+        d3.intoHumanAge();
+    }
+}
